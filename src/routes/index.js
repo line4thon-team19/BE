d499d57f95e61dcec91d492ebc38ce79c1fde582
shinchaerin79 <shinchaerin79@gmail.com>
2025-10-23 15:03:03 +0900

    :sparkles: [FEAT] 웹소켓 초기 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const health = require('./health.route');
+const battle = require('./battle.route');
 
 const router = Router();
 
 router.use('/health', health);
+router.use('/battle', battle);
 
 module.exports = router;
